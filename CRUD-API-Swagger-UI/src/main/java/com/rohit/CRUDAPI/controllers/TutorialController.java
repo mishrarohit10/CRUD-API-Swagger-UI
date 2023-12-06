4cc98191e1b83e47c176537093ec5a0420ba95fd
mishrarohit10 <mishrarohit316x@gmail.com>
2023-12-07 01:20:19 +0530

    working correctly locally

--- CRUD-API-Swagger-UI/src/main/java/com/rohit/CRUDAPI/controllers/TutorialController.java
+++ CRUD-API-Swagger-UI/src/main/java/com/rohit/CRUDAPI/controllers/TutorialController.java
@@ -4,15 +4,22 @@ import com.rohit.CRUDAPI.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class TutorialController {
     @Autowired
     TutorialRepository tutorialRepo;
+
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "OK";
+    }
 
     @GetMapping("/tutorials")
     public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
